0bc0dc883ffc756dce84ac57aae3dfc677cc64c5
StepanovPlaton <mail2platon@yandex.ru>
2022-05-21 10:18:39 +0400

    21.05 Front init

---
+++ frontend/i_want/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthorizationComponent } from './authorization/authorization.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  },
+  { 
+    path: 'auth', 
+    component: AuthorizationComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
